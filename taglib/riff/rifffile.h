a681c1f48a71617f4260295e6dee28df8ca93907
Dmytro Afanasiev <DAfanasiev@luxoft.com>
2019-03-14 13:03:36 +0200

    [WIP] Adds AVI file support
    Adds AVI file class.
    Adds AVI properties class.
    Adds dirty hack for riff file chunks parsing (
      AVI container has AVI Index Entry, that is considered by RIFF File reading as
      invalid chunk, because index entry doesn't have a name, it's first entry is an ID,
      which is out of ASCII chars. So, RIFF::File consider file as invalid, that is not true.)
    Implements extraction of AVI file duration.

--- taglib/riff/rifffile.h
+++ taglib/riff/rifffile.h
@@ -139,6 +139,14 @@ namespace TagLib {
        */
       void removeChunk(const ByteVector &name);
 
+      enum ParseStatus {
+          NO_ERROR,
+          ERROR_INVALID_CHUNK_NAME,
+          ERROR_INVALID_CHUNK_SIZE
+      };
+
+      ParseStatus m_lastError;
+
     private:
       File(const File &);
       File &operator=(const File &);
